9ebe65a4fdf5f5670ba1861b9cc5c9568282f2a9
Matthew Pickering <wyomatthew@hotmail.com>
2023-12-17 19:25:06 -0600

    fix: handling tabs properly
    Signed-off-by: Matthew Pickering <wyomatthew@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 
 use std::io::{self, Read, Write};
 
+static SPACES_PER_TAB: usize = 8;
+
 fn read_all() -> Result<String, Box<dyn std::error::Error>> {
     let mut buf: Vec<u8> = vec!();
 
     let _ = std::io::stdin().read_to_end(&mut buf)?;
 
     Ok(String::from_utf8(buf)?)
+}
+
+fn expand(str_in: &String) -> String {
+    str_in.replace("\t", " ".repeat(SPACES_PER_TAB).as_str())
 }
 
 fn get_max_line_len(input: &String) -> usize {
@@ -32,10 +38,12 @@ fn build_body_str(line: &str, max_line_len: usize) -> String {
 }
 
 fn boxify(input: &String) -> String {
-    let max_line_len: usize = get_max_line_len(input);
-    let split = match input.strip_suffix("\n") {
+    let cleaned_input = expand(input);
+    let max_line_len: usize = get_max_line_len(&cleaned_input);
+
+    let split = match (&cleaned_input).strip_suffix("\n") {
         Some(prefix) => prefix.split("\n"),
-        None => input.split("\n")
+        None => cleaned_input.split("\n")
     };
 
     let mut out: Vec<String> = vec!();
@@ -48,6 +56,42 @@ fn boxify(input: &String) -> String {
     out.join("\n") + "\n"
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{*};
+
+    #[test]
+    fn test_build_top_str() {
+        assert_eq!(build_top_str(0), "╭╮");
+
+        assert_eq!(build_top_str(5), "╭―――――╮");
+    }
+
+    #[test]
+    fn test_build_bottom_str() {
+        assert_eq!(build_bottom_str(0), "╰╯");
+
+        assert_eq!(build_bottom_str(5), "╰―――――╯");
+    }
+
+    #[test]
+    fn test_build_body_str() {
+        assert_eq!(build_body_str("", 0), "││");
+
+        assert_eq!(build_body_str("test", 10), "│test      │");
+
+        assert_eq!(build_body_str("test", 4), "│test│");
+
+    }
+
+    #[test]
+    fn test_expand() {
+        let test_str = String::from("\ttest");
+
+        assert_eq!(expand(&test_str).len(), 4 + SPACES_PER_TAB);
+    }
+}
+
 fn main() -> io::Result<()>{
     let input = match read_all() {
         Ok(input) => input,
@@ -56,7 +100,6 @@ fn main() -> io::Result<()>{
 
     let mut writer = io::BufWriter::new(io::stdout());
     let _ = writer.write_all(boxify(&input).as_bytes());
-    // println!("max line len is: {}", get_max_line_len(&input));
 
     Ok(())
 }
